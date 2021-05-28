925d9a91cdf9e063bd7c314277bae00386654627
Danilo Castro <danilogcastro@hotmail.com>
2021-05-28 11:27:22 -0300

    changed datatype to decimals in movie and made some adjustments to database

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,21 +9,28 @@
 require 'json'
 require 'open-uri'
 
+Movie.destroy_all if Rails.env.development?
+
 puts "Seeding..."
 
-url = 'http://tmdb.lewagon.com/movie/top_rated?page=5'
-movies_serialized = URI.open(url).read
-movies = JSON.parse(movies_serialized)
+25.times do
 
-movies["results"].each do |movie|
+  url = "http://tmdb.lewagon.com/movie/top_rated?page=#{(1..100).to_a.sample}"
+  movies_serialized = URI.open(url).read
+  movies = JSON.parse(movies_serialized)
+
+  movies["results"].each do |movie|
   new_movie = Movie.new(
     title: movie["original_title"],
     overview: movie["overview"],
     poster_url: "https://image.tmdb.org/t/p/w500#{movie['poster_path']}",
     rating: movie["vote_average"]
-  )
-  new_movie.save
-  puts "#{new_movie.id} - #{new_movie.title}"
+    )
+    new_movie.save
+    puts "#{new_movie.id} - #{new_movie.title}"
+  end
+
 end
+
 
 puts "All done!"
